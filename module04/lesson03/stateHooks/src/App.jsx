cfc8f7082de7d3a27cc9571bdd57216e266da7a6
Danilo Paravani <daniloparavani@yahoo.com.br>
2024-07-19 21:54:34 -0300

    useContext

--- module04/lesson03/stateHooks/src/App.jsx
+++ module04/lesson03/stateHooks/src/App.jsx
@@ -5,16 +5,49 @@ import NavegacaoAbas from './components/NavegacaoAbas';
 import Notices from './components/Notices';
 import RegistrationForm from './components/Form';
 
+import { useState, createContext, useContext } from 'react';
+
+const PreferencesContext = createContext();
+
 function App() {
+
+  const [ preferences, setPreferences ] = useState({ theme: 'light', language: 'en'});
+
+  const toggleTheme = () => {
+    setPreferences(currentTheme => ({
+      ...currentTheme,
+      theme: currentTheme.theme === 'light' ? 'dark' : 'light'
+    }));
+  }
+
+  const changeLanguage = (language) => {
+    setPreferences(currentLanguage => ({
+      ...currentLanguage,
+      language: language
+    }))
+  }
+
   return (
     <>
-      <Counter />
-      <RepeatText />
-      <ToDoList />
-      <NavegacaoAbas />
-      <Notices />
-      <RegistrationForm />
+      <PreferencesContext.Provider value={{ preferences, toggleTheme, changeLanguage }}>
+        <div>
+          <Toolbar />
+          <button onClick={toggleTheme}>Trocar o tema</button>
+          <button onClick={() => changeLanguage('pt-br')}>Trocar o idioma = PT-BR</button>
+          <button onClick={() => changeLanguage('en')}>Trocar o idioma - EN</button>
+        </div>
+      </PreferencesContext.Provider>
     </>
+  )
+}
+
+function Toolbar() {
+  const { preferences } = useContext(PreferencesContext);
+
+  return (
+    <div style={{ background: preferences.theme === 'dark' ? 'grey' : 'blue', color: preferences.theme === 'dark' ? 'white' : 'white'}}>
+      Tema utilizado - {preferences.theme}, Idioma utilizado - {preferences.language}
+    </div>
   )
 }
 
